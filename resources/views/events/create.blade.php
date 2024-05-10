f027ae0642eb7945e91435d7d6c64f10a09d9e1c
unknown <joaovitorcavalcante009@gmail.com>
2024-05-10 12:43:34 -0300

    Cadastro de evento pronto

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -4,6 +4,36 @@
 
 @section('content')
 
-    <h1>Criar Evento</h1>
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie seu evento</h1>
+
+        <form action="/events" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+            </div>
+            
+            <div class="form-group">
+                <label for="title">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+            </div>
+            
+            <div class="form-group">
+                <label for="title">O evento é privado:</label>
+                <select class="form-controll" name="private" id="private">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="title">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que será o evento?"></textarea>
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Criar evento">
+        </form>
+    </div>
 
 @endsection 
