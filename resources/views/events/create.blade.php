789207d81032d6c283fbef3736c415b2aa767c10
unknown <joaovitorcavalcante009@gmail.com>
2024-05-10 13:25:38 -0300

    Criar eventos pronto

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,8 +7,13 @@
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Crie seu evento</h1>
 
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
             @csrf
+            <div class="form-group">
+                <label for="image">Imagem do evento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">
+            </div>
+
             <div class="form-group">
                 <label for="title">Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
@@ -21,7 +26,7 @@
             
             <div class="form-group">
                 <label for="title">O evento é privado:</label>
-                <select class="form-controll" name="private" id="private">
+                <select class="form-control" name="private" id="private">
                     <option value="0">Não</option>
                     <option value="1">Sim</option>
                 </select>
